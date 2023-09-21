da1caf227b82bebe24af105117a4f2e403a7b5e1
blur0b0t <jatin.gcp@gmail.com>
2023-09-21 09:01:03 -0700

    mod

--- model/ft_model.sh
+++ model/ft_model.sh
@@ -4,12 +4,18 @@ cd "/home/u131168/mh_one_api/model/intel-extension-for-transformers/workflows/ch
 pip install -r "requirements.txt"
 cd "/home/u131168/mh_one_api/model/intel-extension-for-transformers/workflows/chatbot/fine_tuning/instruction_tuning_pipeline"
 
-export train_file="/home/u131168/mh_one_api/data/train_split/25_50k.csv"
+pip install git+https://github.com/huggingface/transformers
+
+export train_file="/home/u131168/mh_one_api/data/train_split/full_traind.csv"
+
 export model_path="google/flan-t5-xl"
+export checkpoint_path="/home/u131168/mh_one_api/model/ft_models/flan-t5-xl_peft_finetuned_model/checkpoint-6000"
+
 export output_dir="/home/u131168/mh_one_api/model/ft_models/flan-t5-xl_peft_finetuned_model"
 
 python finetune_seq2seq.py \
         --model_name_or_path $model_path \
+        --resume_from_checkpoint $checkpoint_path \
         --bf16 True \
         --train_file $train_file \
         --per_device_train_batch_size 2 \
@@ -19,7 +25,7 @@ python finetune_seq2seq.py \
         --learning_rate 1.0e-5 \
         --warmup_ratio 0.03 \
         --weight_decay 0.0 \
-        --num_train_epochs 5 \
+        --num_train_epochs 1 \
         --logging_steps 10 \
         --save_steps 2000 \
         --save_total_limit 2 \
